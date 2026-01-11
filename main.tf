7454ea3769b00255ceafd933717cb019f307c476
Dinesh Vinnakota <dinesh.vinnakota02@gmail.com>
2026-01-11 13:12:04 -0500

    Initial

---
+++ main.tf
@@ -0,0 +1,27 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
